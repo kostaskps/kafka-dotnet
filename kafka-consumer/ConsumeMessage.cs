8b18331fe857bdd313cb45b8024cfd93955a201a
Kostas Kapsalis <kapsalisk.develop@hotmail.com>
2024-03-17 22:20:27 +0200

    Add project files.

---
+++ kafka-consumer/ConsumeMessage.cs
@@ -0,0 +1,50 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kafka_consumer
+{
+    internal class ConsumeMessage
+    {
+        public void ReadMessage()
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "localhost:19092",
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                ClientId = "my-app",
+                GroupId = "my-group",
+                BrokerAddressFamily = BrokerAddressFamily.V4,
+            };
+
+            using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+
+            consumer.Subscribe("kafka.learning.orders");
+
+            try
+            {
+                while (true)
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+
+                    var consumeResult = consumer.Consume();
+                    Console.WriteLine($"Message received from {consumeResult.TopicPartitionOffset}: {consumeResult.Message.Value}");
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // The consumer was stopped via cancellation token.
+            }
+            finally
+            {
+                consumer.Close();
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ kafka-consumer/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace kafka_consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!, This is Kafka Consumer Application");
+            
+            ConsumeMessage consumeMessage = new ConsumeMessage();
+            consumeMessage.ReadMessage();
+        }
+    }
+}
